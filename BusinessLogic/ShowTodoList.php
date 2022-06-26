6381efe1fef25a7eb95752d449c0d73684e7761c
Muhammad Fikri <fikri2347@gmail.com>
2022-06-26 20:02:42 +0700

    Membuat business logic & test show todo list

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -4,5 +4,11 @@
  * Menampilkan todo di list
  */
 function showTodoList(){
+  global $todoList;
 
+  echo "TODOLIST" . PHP_EOL;
+
+  foreach($todoList as $number => $todo){
+    echo "$number. $todo" . PHP_EOL;
+  }
 }
