eb0ba6b09507d4d13b6534a9cecc27fddd1c023d
Muhammad Fikri <fikri2347@gmail.com>
2022-06-26 21:10:31 +0700

    Membuat view & test add todo list

---
+++ Test/TestViewAddTodoList.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once './View/ViewAddTodoList.php';
+require_once './BusinessLogic/AddTodoList.php';
+require_once './BusinessLogic/ShowTodoList.php';
+
+addTodoList("Muhammad");
+addTodoList("Fikri");
+addTodoList("19");
+
+viewAddTodoList();
+showTodoList();
+
+viewAddTodoList();
+showTodoList();
+
--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,5 +1,15 @@
 <?php
+require_once './Model/TodoList.php';
+require_once './BusinessLogic/AddTodoList.php';
+require_once './View/ViewShowTodoList.php';
 
 function viewAddTodoList(){
+  echo "Menambah TodoList".PHP_EOL;
+  $todo = input("Masukan Todo (Tekan x untuk batal)");
 
+  if($todo == "x"){
+    echo "Batal menambah todo" . PHP_EOL;
+  }else{
+    addTodoList($todo);
+  }
 }
