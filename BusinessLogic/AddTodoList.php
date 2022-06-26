96e11e124de146b79220e779831b89be09631655
Muhammad Fikri <fikri2347@gmail.com>
2022-06-26 20:22:00 +0700

    Membuat business logic & test add todo list

--- BusinessLogic/AddTodoList.php
+++ BusinessLogic/AddTodoList.php
@@ -3,6 +3,9 @@
 /**
  * Menambahkan todo ke list
  */
-function addTodoList(){
+function addTodoList(string $todo){
+  global $todoList;
 
+  $number = count($todoList) + 1;
+  $todoList[$number] = $todo;
 }
